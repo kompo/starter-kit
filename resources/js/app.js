4b6d0ced62c2fde378064915a7cf37bf358017e3
basslhm <contact@kompo.io>
2021-09-05 09:39:10 -0400

    Kompo installation + tailwind + importing JS and SCSS + building assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue/dist/vue'
+window.Vue = Vue
+
+//Requiring vue-kompo after Vue has been set
+require('vue-kompo');
+//require('kompo-ckeditor'); //add any optional kompo plugins here
+
+//Booting Vue
+const app = new Vue({ el: '#app' });
